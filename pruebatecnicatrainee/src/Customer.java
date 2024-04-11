b2185b2079dbd4cc905049dab4210d710507e45d
Elias <evelazquez2604@gmail.com>
2024-04-11 18:53:36 -0300

    feat: Add Customer class

---
+++ pruebatecnicatrainee/src/Customer.java
@@ -0,0 +1,35 @@
+public class Customer {
+    int customer_id;
+    int dni;
+    String name;
+    String lastName;
+
+    public Customer(
+        int customer_id,
+        int dni,
+        String name,
+        String lastName
+    ){
+        this.customer_id = customer_id;
+        this.dni = dni;
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public int getCustomer_id(){
+        return this.customer_id;
+    }
+
+    public int getDni(){
+        return this.dni;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getLastName(){
+        return this.lastName;
+    }
+
+}
